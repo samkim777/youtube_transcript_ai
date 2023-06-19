a793eb3b25f945833efbb9de144fa71eb70416a2
samkim <milk3531@hotmail.com>
2023-06-18 22:37:26 -0700

    Init
    get transcript given youtube id

---
+++ main.py
@@ -0,0 +1,23 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+video_id = '682dDcNcCNM'
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+script = []
+# Grab all the texts 
+for dialogue in transcript:
+  script.append(dialogue['text'])
+
+
+
+# Filter out any descriptive words like [Music] or [Applause]
+filtered_text = []
+for sentence in script:
+  filtered_text.append(sentence.replace("'",'"'))
+
+dialogue = []
+for string in filtered_text:
+    if string not in ['[Music]', '[Applause]']:
+        dialogue.append(string)
+
+
+print(" ".join(dialogue))
